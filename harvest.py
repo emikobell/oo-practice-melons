de7170c61deca3470ecbf2e5fcb39b583ec8a802
Emiko Bell <emikoelizabethbell@gmail.com>
2023-04-12 13:16:41 -0700

    Completed update_code function

--- harvest.py
+++ harvest.py
@@ -18,19 +18,17 @@ class MelonType:
         self.name = name
         self.pairings = []
 
-        # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
         self.pairings.append(pairing)
 
-        # Fill in the rest
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
-        # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
